ba59eeb0ce8000df96d9c1479b029e4f84b18883
banki.gabor <banki.gabor@28-DIAK7>
2024-10-07 12:43:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using triatlon241007;
+using System.Text;
+
+List<Versenyzo> versenyzok = [];
+
+using StreamReader sr = new(
+    path: @"forras.txt",
+    encoding: Encoding.UTF8);
+while (!sr.EndOfStream) versenyzok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"A versenyt befejező versenyzők száma: {versenyzok.Count}");
+
+var elitjuniorversenyzok = versenyzok.Count(v => v.Korcsoport == "elit junior");
+Console.WriteLine($"\nElit junior kategóriában versenyzők száma: {elitjuniorversenyzok}");
+
+var ferfiatlag = versenyzok.Where(v => v.Nem == "f").Average(a => a.Kor);
+Console.WriteLine($"\nA férfi versenyzők átlag életkora: {ferfiatlag:0.00}");
+
+var uszasatlag = versenyzok.Where(v => v.Korcsoport == "20-24").Average(a => a.Uszas.Hour);
+Console.WriteLine($"\nA 20-24 éves korcsoportban átlagos úszási idő: {uszasatlag}");
+
+
+var ferfibefejezett = versenyzok.Count(v => v.Nem == "f");
+var noibefejezett = versenyzok.Count(v => v.Nem == "n");
+Console.WriteLine($"\nA versenyt befejező férfiak száma: {ferfibefejezett}\nA versenyt befejező nők száma: {noibefejezett}");
---
+++ Versenyzo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triatlon241007
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int Szulev { get; set; }
+        public int Rajtszam { get; set; }
+        public string Nem { get; set; }
+        public string Korcsoport { get; set; }
+        public DateTime Uszas {  get; set; }
+        public DateTime Elsodepo { get; set; }
+        public DateTime Kerekpar { get; set; }
+        public DateTime Masodikdepo { get; set; }
+        public DateTime Futas { get; set; }
+        public int Kor => (int)(2024 - Szulev);
+
+        public override string ToString()
+        {
+            return $"{Nev}: SZületett: {Szulev}, Rajtszám: {Rajtszam}, Neme: {Nem}, Korcsoport: {Korcsoport}, Időeredmények(Úszás, első depóban töltött idő, kerékpározás, második depóban töltött idő, futás): {Uszas}; {Elsodepo}; {Kerekpar}; {Masodikdepo}; {Futas} ";
+        }
+
+
+        public Versenyzo(string row)
+        {
+            var v = row.Split(';');
+            Nev = v[0];
+            Szulev = int.Parse(v[1]);
+            Rajtszam = int.Parse(v[2]);
+            Nem = v[3];
+            Korcsoport = v[4];
+            Uszas = DateTime.ParseExact(v[5], "HH:mm:ss", CultureInfo.InvariantCulture);
+            Elsodepo = DateTime.ParseExact(v[6], "HH:mm:ss", CultureInfo.InvariantCulture);
+            Kerekpar = DateTime.ParseExact(v[7], "HH:mm:ss", CultureInfo.InvariantCulture);
+            Masodikdepo = DateTime.ParseExact(v[8], "HH:mm:ss", CultureInfo.InvariantCulture);
+            Futas = DateTime.ParseExact(v[9], "HH:mm:ss", CultureInfo.InvariantCulture);
+        }
+    }
+}
